3ae31ee7e2c20024d3a4038810ecfbdb0031040a
Cyrille Rossant <rossant@users.noreply.github.com>
2023-04-03 11:21:10 +0200

    WIP: unity integration

--- run.sh
+++ run.sh
@@ -1,1 +1,6 @@
-python -m http.server
+# to generate local https certificate:
+# - sudo apt install mkcert
+# - mkcert -install
+# - mkcert localhost
+
+http-server -p 8000 -S -C localhost.pem -K localhost-key.pem
