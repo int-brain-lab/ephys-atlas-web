c572da5bf0117093c0f3adb1a975fe6204bec8f8
Cyrille Rossant <rossant@users.noreply.github.com>
2023-06-19 14:04:37 +0200

    Add dispatcher module

---
+++ js/dispatcher.js
@@ -0,0 +1,89 @@
+export { Dispatcher };
+
+// import { downloadJSON } from "./utils.js";
+
+
+
+/*************************************************************************************************/
+/* Dispatcher                                                                                    */
+/*************************************************************************************************/
+
+class Dispatcher {
+    constructor() {
+        this.el = document.getElementById('dispatcher');
+    }
+
+    emit(name, source, data) {
+        const ev = new CustomEvent(name, {
+            detail: {
+                source: source,
+                data: data,
+            },
+        });
+        this.el.dispatchEvent(ev);
+    }
+
+    on(name, callback) {
+        this.el.addEventListener(name, (ev) => { return callback(ev.detail.data); });
+    }
+
+    slice(source, axis, idx) {
+        // when a slice changes
+        this.emit("slice", source, { "axis": axis, "idx": idx });
+    }
+
+    highlight(source, idx) {
+        // when a region is highlighted
+        this.emit("highlight", source, { "idx": idx });
+    }
+
+    toggle(source, idx) {
+        // when a region is added or removed from the selection
+        this.emit("toggle", source, { "idx": idx });
+    }
+
+    clear(source,) {
+        // when all regions are deselected
+        this.emit("clear", source, {});
+    }
+
+    bucket(source, uuid_or_alias) {
+        // when a bucket is selected
+        this.emit("bucket", source, { "uuid_or_alias": uuid_or_alias });
+
+    }
+
+    feature(source, fname) {
+        // when a feature is selected
+        this.emit("feature", source, { "fname": fname });
+    }
+
+    cmap(source, name) {
+        // when the colormap is changed
+        this.emit("cmap", source, { "name": name });
+    }
+
+    stat(source, name) {
+        // when the stat is changed
+        this.emit("stat", source, { "name": name });
+    }
+
+    search(source, text) {
+        // when search text is changed
+        this.emit("search", source, { "text": text });
+    }
+
+    mapping(source, name) {
+        // when the mapping is changed
+        this.emit("mapping", source, { "name": name });
+    }
+
+    cmap_range(source, vmin, vmax) {
+        // when the colormap range changes
+        this.emit("cmap_range", source, { "vmin": vmin, "vmax": vmax });
+    }
+
+
+};
+
+
