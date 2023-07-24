b63e26812bc5f97c136193a946b41ebacca9f48c
Cyrille Rossant <rossant@users.noreply.github.com>
2023-07-24 17:39:18 +0200

    Update upload script

--- upload_data.sh
+++ upload_data.sh
@@ -1,5 +1,5 @@
 rsync -avzh data/css iblviz:/var/www/ibl_website/atlas2/data/
 rsync -avzh data/json iblviz:/var/www/ibl_website/atlas2/data/
-rsync -avzh data/features iblviz:/var/www/ibl_website/atlas2/data/
+#rsync -avzh data/features iblviz:/var/www/ibl_website/atlas2/data/
 
 #rsync -avzh Build iblviz:/var/www/ibl_website/atlas2/
